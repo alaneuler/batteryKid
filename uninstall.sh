16e98b6e019335dedd68ee3a2ec0640f4fc448ee
alaneuler <alaneuler@qq.com>
2023-02-04 20:15:24 +0800

    add uninstallation script

--- uninstall.sh
+++ uninstall.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 sudo launchctl unload /Library/LaunchDaemons/me.alaneuler.batteryKid.PrivilegeHelper.plist
 sudo rm -rf /Library/LaunchDaemons/me.alaneuler.batteryKid.PrivilegeHelper.plist
 sudo rm -rf /Library/PrivilegedHelperTools/me.alaneuler.batteryKid.PrivilegeHelper
